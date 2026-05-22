31e9d183e337f9d96e7d6a493c86d0b453c21da1
Oliver <opaligoric@gmail.com>
2026-05-22 22:25:01 +0200

    Added data seeding and connstr moved in appconfg

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -2,13 +2,12 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
 
-var connString = "Data Source=GameStore.db";
-
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.AddGameStoreDb();
 
 var app = builder.Build();
 
